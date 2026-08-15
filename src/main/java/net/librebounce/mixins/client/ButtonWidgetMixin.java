4d3c6e5d175e98a5f864874bc719f2e9ff46785c
PC <thatonecoder@tutamail.com>
2026-08-15 01:09:14 +0100

    Change a tad more

--- src/main/java/net/librebounce/mixins/client/ButtonWidgetMixin.java
+++ src/main/java/net/librebounce/mixins/client/ButtonWidgetMixin.java
@@ -4,8 +4,6 @@ import io.github.axolotlclient.rendering.DrawUtil;
 import io.github.axolotlclient.rendering.font.DefaultFont;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.TextRenderer;
-import net.minecraft.client.render.platform.GlStateManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
