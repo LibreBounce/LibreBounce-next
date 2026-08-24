3e9b8e1c494a1ea66016fcaad83f1e1ba4a21ae6
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-08-24 03:17:00 +0100

    Improve ClickingSettings' code

--- src/main/kotlin/net/librebounce/features/module/base/settings/ClickingSettings.kt
+++ src/main/kotlin/net/librebounce/features/module/base/settings/ClickingSettings.kt
@@ -2,35 +2,28 @@ package net.librebounce.features.module.base.settings
 
 import net.librebounce.config.Configurable
 import net.librebounce.features.module.base.Module
-import net.librebounce.utils.extensions.safeDiv
+import net.librebounce.utils.kotlin.RandomUtils.nextRateMilliseconds
 import net.librebounce.utils.timing.MSTimer
-import kotlin.math.roundToInt
 
-open class ClickingSettings(owner: Module, prefix: String = "", generalApply: () -> Boolean = { true }): Configurable("ClickingSettings") {
-    private val cps by intRange(prefix + "CPS", 8..12, 0..50) { generalApply() }
-    private val clicksAtATime by intRange(prefix + "ClicksAtATime", 1..1, 0..5) { generalApply() }
+open class ClickingSettings(owner: Module, prefix: String = "", shouldApply: Boolean = true): Configurable(owner.name) {
+    private val cps by intRange(prefix + "CPS", 8..12, 0..50) { shouldApply }
+    private val clicksAtATime by intRange(prefix + "ClicksAtATime", 1..1, 0..5) { shouldApply }
 
     init {
         owner.addValues(this.values)
     }
 
-    private var delay = randomClickDelay(cps)
+    private var delay = nextRateMilliseconds(cps)
     private var lastClick = MSTimer()
     var clicks = clicksAtATime.random()
 
     fun canClick(): Boolean {
         if (lastClick.hasTimePassed(delay)) {
             clicks = clicksAtATime.random()
-            delay = randomClickDelay(cps)
+            delay = nextRateMilliseconds(cps)
             lastClick.reset()
             return true
         }
         return false
-    }
-
-    fun randomClickDelay(cps: IntRange): Int {
-        val minDelay = 1000 safeDiv cps.first
-        val maxDelay = 1000 safeDiv cps.last
-        return (Math.random() * (minDelay - maxDelay) + maxDelay).roundToInt()
     }
 }
