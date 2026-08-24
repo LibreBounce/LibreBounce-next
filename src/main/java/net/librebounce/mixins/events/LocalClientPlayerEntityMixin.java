1520a01cccb011f6ab56a008cbc4fb9c4a044685
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-08-24 07:28:02 +0100

    ...

--- src/main/java/net/librebounce/mixins/events/LocalClientPlayerEntityMixin.java
+++ src/main/java/net/librebounce/mixins/events/LocalClientPlayerEntityMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalClientPlayerEntity.class)
-public abstract class LocalClientPlayerEntityMixin {
+public abstract class LocalClientPlayerEntityMixin extends Entity {
 	@Shadow
 	public Input input;
 
