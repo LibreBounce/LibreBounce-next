ec97aae4fce24b4e6c77de8c98d5e63b93a3a06c
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-08-24 04:29:04 +0100

    Attempt to simplify the ClickingSettings code

--- src/main/kotlin/net/librebounce/features/module/base/settings/ClickingSettings.kt
+++ src/main/kotlin/net/librebounce/features/module/base/settings/ClickingSettings.kt
@@ -17,13 +17,9 @@ open class ClickingSettings(owner: Module, prefix: String = "", shouldApply: Boo
     private var lastClick = MSTimer()
     var clicks = clicksAtATime.random()
 
-    fun canClick(): Boolean {
-        if (lastClick.hasTimePassed(delay)) {
-            clicks = clicksAtATime.random()
-            delay = nextRateMilliseconds(cps)
-            lastClick.reset()
-            return true
-        }
-        return false
+    fun canClick() = lastClick.hasTimePassed(delay).also {
+        clicks = clicksAtATime.random()
+        delay = nextRateMilliseconds(cps)
+        lastClick.reset()
     }
 }
